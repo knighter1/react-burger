2c1be9cfd6386a14c5b4e5b8a90496177fe4570f
Alexander <knighter@rambler.ru>
2021-06-09 18:52:53 +0900

    Calc order cost by use reduce hook

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './BurgerConstructor.module.css';
 import IngredientsList from '../IngredientsList/IngredientsList';
@@ -12,42 +12,62 @@ interface IBurgerConstructorProps
     items: IngredientData[];
 }
 
-interface IBuns
+interface IComponents
 {
     first?: IngredientData,
+    sortedItems: IngredientData[],
+    innerItems: IngredientData[],
     last?: IngredientData
+}
+
+interface IOrderCost
+{
+    value: number;
+}
+
+function orderCostReducer(state: IOrderCost, items: IngredientData[]): IOrderCost
+{
+    const value: number = items.reduce((acc, item) => {
+        return acc + item.price;
+    }, 0);
+
+    return {value: value};
 }
 
 const BurgerConstructor = ({items}: IBurgerConstructorProps) =>
 {
     const PLACE_ORDER_ENDPOINT: string = 'https://norma.nomoreparties.space/api/orders';
-    
-    useEffect(() => {
-        const first = items.find(element => element.type === IngredientTypes[IngredientTypes.bun]);
-        const last = first;
-        last && items.push(last);
-        last && setBuns({first: first, last: last });
-    }, [items]);
 
-    const [currentItems, setItems] = useState(items);
+    const [currentItems, setCurrentItems] = useState(items);
+
+    const [components, setComponents] =  useState<IComponents>({sortedItems: [], innerItems: []});
+
+    useEffect(() => {
+        const first = currentItems.find(element => element.type === IngredientTypes[IngredientTypes.bun]);
+        const last = first;
+        const innerItems = currentItems.filter((element) => element.type !== IngredientTypes[IngredientTypes.bun]);
+        let sortedItems: IngredientData[] = [...innerItems];
+
+        if (first) sortedItems = [first, ...sortedItems];
+        last && sortedItems.push(last);
+
+        setComponents({first: first, sortedItems: sortedItems, innerItems: innerItems, last: last });
+    }, [currentItems]);
+
+    console.log(components);
+
     const [modalState, setModalState] = useState(false);
-    const [buns, setBuns] =  useState<IBuns>({});
     const [orderNumber, setOrderNumber] =  useState(0);
 
-    const onRemoveItem = (id: string): void => setItems(currentItems.filter(element => element._id !== id));
+    useEffect(() => {
+        orderCostDispatch(components.sortedItems);
+    }, [components.sortedItems]);
 
-    const totalOrderCost: number = currentItems.reduce((acc, item) => {
-        return acc + item.price;
-    }, 0);
-
-    const innerItems = currentItems.filter((element) => element.type !== IngredientTypes[IngredientTypes.bun]);
+    const onRemoveItem = (id: string): void => setCurrentItems(components.sortedItems.filter(element => element._id !== id));
 
     const placeOrder = () => {
         
-        const ingredients = currentItems.map(item => item._id);
-        
-        //currentItems.forEach(item => console.log(item.name));
-        //console.log("ingredients: ", ingredients);
+        const ingredients = components.sortedItems.map(item => item._id);
 
         fetch(PLACE_ORDER_ENDPOINT, {
             method: 'POST',
@@ -66,17 +86,19 @@ const BurgerConstructor = ({items}: IBurgerConstructorProps) =>
         .catch(error => console.error(`Order placing error: ${error}`));
     }
 
+    const [orderCost, orderCostDispatch] = useReducer(orderCostReducer, {value: 0}, undefined);
+
     return (
         <>
             <section className={`${styles.section} ml-10`}>
                 <div className={`${styles.scrollableList} pt-25`}>
-                    {buns.first && <IngredientsListItem type="top" data={buns.first} />}
-                    {currentItems.length && <IngredientsList items={innerItems} onRemoveItemHandle={(id: string) => onRemoveItem(id)} />}
-                    {buns.last && <IngredientsListItem type="bottom" data={buns.last} />}
+                    {components.first && <IngredientsListItem type="top" data={components.first} />}
+                    {components.innerItems.length > 0 && <IngredientsList items={components.innerItems} onRemoveItemHandle={(id: string) => onRemoveItem(id)} />}
+                    {components.last && <IngredientsListItem type="bottom" data={components.last} />}
                 </div>
 
                 <div className={`mt-10 mr-4 ${styles.commitOrderWrapper}`}>
-                    <span className="text text_type_digits-medium mr-2">{totalOrderCost}</span>
+                    <span className="text text_type_digits-medium mr-2">{orderCost.value}</span>
                     <CurrencyIcon type="primary" />
                     <div className={`ml-10 ${styles.buttonWrapper}`}>
                         <Button type="primary" size="large" onClick={() => placeOrder()}>

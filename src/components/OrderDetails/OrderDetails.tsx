fd1e18cc44e5360d7810b03c3bf952487baeb1d9
Alexander <knighter@rambler.ru>
2021-07-26 18:23:49 +0900

    Order placing loader

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -7,15 +7,29 @@ export const OrderDetails = (): JSX.Element =>
 {
     const orderId: number = useSelector((store: IStore) => store.order.orderId);
 
+    let content;
+    if (orderId)
+        content = (
+            <>
+                <div className={`${styles.orderId} text text_type_digits-large pt-30`}>
+                    <span>{orderId}</span>
+                </div>
+                <div className="text text_type_main-medium mt-8">идентификатор заказа</div>
+                <img className="mt-15" src={orderDoneImg} alt="Заказ размещен" />
+                <div className="text text_type_main-default mt-15">Ваш заказ начали готовить</div>
+                <div className="text text_type_main-default text_color_inactive mt-2 mb-30">Дождитесь готовности на орбитальной станции</div>
+            </>);
+    else
+        content = (
+            <>
+                <div className={`text text_type_main-large ${styles.forming}`}>Формирование заказа</div>
+                <div className={`text text_type_main-medium ${styles.forming}`}>Ожидайте...</div>
+            </>
+        )
+
     return (
         <div className={`${styles.orderDetail} modal-сontent`}>
-            <div className={`${styles.orderId} text text_type_digits-large pt-30`}>
-                <span>{orderId}</span>
-            </div>
-            <div className="text text_type_main-medium mt-8">идентификатор заказа</div>
-            <img className="mt-15" src={orderDoneImg} alt="Заказ размещен" />
-            <div className="text text_type_main-default mt-15">Ваш заказ начали готовить</div>
-            <div className="text text_type_main-default text_color_inactive mt-2 mb-30">Дождитесь готовности на орбитальной станции</div>
+            {content}
         </div>
     );
 }

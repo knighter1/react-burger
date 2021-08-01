2e0ffb7d9af577340420722807553e553908259b
Alexander <knighter@rambler.ru>
2021-08-02 00:20:46 +0900

    Typed reset password actions

--- src/redux/actions/resetPassword.ts
+++ src/redux/actions/resetPassword.ts
@@ -2,13 +2,37 @@ export const RESET_PASSWORD_REQUEST = 'RESET_PASSWORD_REQUEST';
 export const RESET_PASSWORD_SUCCESS = 'RESET_PASSWORD_SUCCESS';
 export const RESET_PASSWORD_ERROR = 'RESET_PASSWORD_ERROR';
 
+export interface IResetPasswordRequestAction {
+    readonly type: typeof RESET_PASSWORD_REQUEST;
+}
+
+export interface IResetPasswordSuccessAction {
+    readonly type: typeof RESET_PASSWORD_SUCCESS;
+    readonly message: string;
+}
+
+export interface IResetPasswordErrorAction {
+    readonly type: typeof RESET_PASSWORD_ERROR;
+}
+
+export type TResetPasswordActions =
+    IResetPasswordRequestAction |
+    IResetPasswordSuccessAction |
+    IResetPasswordErrorAction;
+
+export const resetPasswordRequest = (): IResetPasswordRequestAction => ({ type: RESET_PASSWORD_REQUEST });
+
+export const resetPasswordSuccess = (message: string): IResetPasswordSuccessAction => ({ type: RESET_PASSWORD_SUCCESS, message: message });
+
+export const resetPasswordError = (): IResetPasswordErrorAction => ({ type: RESET_PASSWORD_ERROR });
+
 export function resetPassword(password: string, token: string, history: any)
 {
     const END_POINT = 'https://norma.nomoreparties.space/api/password-reset/reset';
     
     return function(dispatch: Function)
     {
-        dispatch({ type: RESET_PASSWORD_REQUEST });
+        dispatch(resetPasswordRequest());
         
         fetch(END_POINT, {
             method: 'POST',
@@ -24,11 +48,11 @@ export function resetPassword(password: string, token: string, history: any)
             return Promise.reject(`Status ${response.status}`);
         })
         .then(responseObj => {
-            dispatch({ type: RESET_PASSWORD_SUCCESS, message: responseObj.message, success: responseObj.success });
+            dispatch(resetPasswordSuccess(responseObj.message));
             history.replace('/login');
         })
         .catch(error => {
-            dispatch({ type: RESET_PASSWORD_ERROR });
+            dispatch(resetPasswordError());
             console.error(`Reset password error: ${error}`)
         });
     }
